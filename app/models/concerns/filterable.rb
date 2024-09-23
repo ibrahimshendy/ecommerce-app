b7b1c9f6e45d0b80607fd7e3f2fad78c9ba3e10c
Ibrahim Shendy <4webcoding@gmail.com>
2024-09-23 09:12:54 +0300

    Add filterable concern for pretty filter conditions

---
+++ app/models/concerns/filterable.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Filterable
+  extend ActiveSupport::Concern
+
+  included do
+    def self.when(passes, lambda_filter)
+      unless passes
+        return self
+      end
+
+      lambda_filter.()
+    end
+
+    def self.filter(params)
+      limit = params[:limit].present? ? params[:limit].to_i : 10
+
+      self.when(params[:id].present?, -> { self.where(id: params[:id].to_i) })
+          .when(params[:sort].present?, -> { self.order(sort: params[:sort].to_s.downcase) })
+          .limit(limit)
+          .offset((params[:page].to_i > 0 ? params[:page].to_i - 1 : 0) * limit)
+    end
+
+  end
+end
