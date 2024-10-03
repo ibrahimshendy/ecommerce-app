1cf2ece10aeb27827926df2ea42b3405bc200650
Ibrahim Shendy <4webcoding@gmail.com>
2024-10-03 23:54:59 +0300

    Redirect to dashboard after admin logged in successfully

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,6 +2,10 @@ class Admin::SessionsController < Devise::SessionsController
   respond_to :html
   before_action :configure_sign_in_params, only: [:create]
 
+  def after_sign_in_path_for(resource)
+    admin_dashboard_path
+  end
+
   protected
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:admin, keys: [:email, :password])
