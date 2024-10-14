e229a2a05f8ace012e0555541fe5c8a46d01e9e1
Ibrahim Shendy <4webcoding@gmail.com>
2024-10-14 15:36:47 +0300

    Add helper method inside application controller for simply highlight active link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def current_active_path?(target_controller, allowed_actions = %w(index show new create edit update))
+    params[:controller] == target_controller && allowed_actions.include?(params[:action])
+  end
 end
