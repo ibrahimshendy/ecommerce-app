bcecf928ee9bf895f60e907374759a43b2332a01
Ibrahim Shendy <4webcoding@gmail.com>
2024-10-14 15:39:05 +0300

    Allowed new alerts types

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,6 +1,8 @@
 class Admin::BaseController < ActionController::Base
   layout 'admin/layout'
 
+  add_flash_types :success, :warning, :danger
+
   respond_to :html
   before_action :authenticate_admin!
 end
